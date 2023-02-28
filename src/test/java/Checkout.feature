4ad3f5366d2910374af3cd0a56b298b9f38cdfc4
Manojk729 <manoj.mk7296@gmail.com>
2023-02-28 12:22:29 +0530

    Just Started

---
+++ src/test/java/Checkout.feature
@@ -0,0 +1,9 @@
+Feature: Myntra
+  Scenario: Verify checkout functionality
+    Given Open the application
+    When Click on Search Bar
+    And  Enter the name of Product
+    And Scroll to the Product
+    And Select the Product
+    And Click on Add to Cart Button
+    Then Login page should appear
---
+++ src/test/java/stepDefinitons/Myntra.java
@@ -0,0 +1,43 @@
+package stepDefinitons;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Myntra {
+    @Given("Open the application")
+    public void open_the_application() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("Click on Search Bar")
+    public void click_on_search_bar() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("Enter the name of Product")
+    public void enter_the_name_of_product() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("Scroll to the Product")
+    public void scroll_to_the_product() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("Select the Product")
+    public void select_the_product() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("Click on Add to Cart Button")
+    public void click_on_add_to_cart_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("Login page should appear")
+    public void login_page_should_appear() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
